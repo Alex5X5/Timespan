769fdd6d1ab141ac42625ed1b072313e8b2d986a
alex <alex.25.lehnert@gmail.com>
2026-05-13 21:42:48 +0200

    Added RedirectionService. Added Timer Page. Added Views Folder in Styles for Styles secific to one view.

---
+++ Timespan.GUI/Services/RedirectionService.cs
@@ -0,0 +1,22 @@
+﻿namespace Timespan.GUI.Services; 
+
+internal class RedirectionService {
+
+
+	
+	internal static void GetRedirectionAnchor<T>() {
+		
+	}
+
+	internal static void GetRedirectionAnchor<T>(string key) {
+		
+	}
+}
+
+
+internal class RedirectionAnchor<T> {
+
+	public T currentModel { get; set; }
+
+	public delegate string ModelChanged();
+}
---
+++ Timespan.GUI/Views/TimerPage.axaml.cs
@@ -0,0 +1,11 @@
+namespace Timespan.GUI;
+
+using Avalonia.Controls;
+
+public partial class TimerPage : UserControl
+{
+    public TimerPage()
+    {
+        InitializeComponent();
+    }
+}
