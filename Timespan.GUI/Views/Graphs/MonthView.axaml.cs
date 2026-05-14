91b3e0cb8927bb0568d84a8f203c0bfe1daa1eb7
alex <alex.25.lehnert@gmail.com>
2026-05-14 18:57:26 +0200

    Added Graph Panels.

---
+++ Timespan.GUI/Views/Graphs/MonthView.axaml.cs
@@ -0,0 +1,11 @@
+namespace Timespan.GUI.Views.Graphs;
+
+using Avalonia.Controls;
+
+public partial class MonthView : UserControl
+{
+    public MonthView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Timespan.GUI/Views/Graphs/WeekView.axaml.cs
@@ -0,0 +1,11 @@
+namespace Timespan.GUI.Views.Graphs;
+
+using Avalonia.Controls;
+
+public partial class WeekView : UserControl
+{
+    public WeekView()
+    {
+        InitializeComponent();
+    }
+}
