ea9664d0635de88f5f73bba5378649f94f5e68ea
Alexander Lehnert <alexander.lehnert@develappers.de>
2026-05-22 14:24:35 +0200

    Updated DayView. Added DayColumn.

---
+++ Timespan.GUI/ViewModels/Graphs/DayCoumnViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Timespan.GUI.ViewModels.Graphs;
+
+public partial class DayCoumnViewModel : ViewModelBase, IGraphsViewChild {
+
+	[ObservableProperty]
+	private bool[] showColumn = new bool[24];
+
+	public DayCoumnViewModel() : base() {
+		for(int i=0; i<showColumn.Length; i++) {
+			if (i < 6) { 
+				showColumn[i] = false;
+			} else if(i > 17) {
+				showColumn[i] = false;
+			} else {
+				showColumn[i] = true;
+			}
+		};
+	}
+
+	public async Task GetTasksAsync() {
+
+	}
+
+	public string GetDateString() {
+		return "Montag 14.3.";
+	}
+}
--- Timespan.GUI/ViewModels/Graphs/DayViewModel.cs
+++ Timespan.GUI/ViewModels/Graphs/DayViewModel.cs
@@ -1,12 +1,42 @@
-﻿namespace Timespan.GUI.ViewModels.Graphs;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
-public class DayViewModel : ViewModelBase, IGraphsViewChild {
+namespace Timespan.GUI.ViewModels.Graphs;
 
-	public DayViewModel() : base() {
+public partial class DayViewModel : ViewModelBase, IGraphsViewChild {
+
+
+	public ObservableCollection<TestItem> TableItems { set; get; } = [];
+
+    [ObservableProperty]
+    private GridLength[] columnWidths = new[]
+    {
+        new GridLength(1, GridUnitType.Star),
+        new GridLength(1, GridUnitType.Pixel)
+    };
+
+
+    public DayViewModel() : base() {
+		//for(int i=0; i<showColumn.Length; i++) {
+		//	if (i < 6) { 
+		//		showColumn[i] = new(0, GridUnitType.Star);
+		//	} else if(i > 17) {
+		//		showColumn[i] = new(0, GridUnitType.Star);
+		//	} else {
+		//		showColumn[i] = new(1, GridUnitType.Star);
+		//	}
+		//};
+	}
+
+
+    public async Task GetTasksAsync() {
 
 	}
 
 	public string GetDateString() {
 		return "Montag 14.3.";
 	}
+
+	public record TestItem();
 }
