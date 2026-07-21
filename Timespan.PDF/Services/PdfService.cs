ac571948acdc18408b0e19230676939575e93217
alex <alex.25.lehnert@gmail.com>
2026-07-21 08:59:31 +0200

    Reverted the Changes to PdfService where exporting was made to run in parallel. Updated OutputReadableIndexers.pdf.

--- Timespan.PDF/Services/PdfService.cs
+++ Timespan.PDF/Services/PdfService.cs
@@ -142,44 +142,41 @@ public unsafe partial class PdfService : IPdfService, IDisposable {
 		long totalWeekSeconds = 0;
 		string query = "";
 		string value = "";
-		Parallel.ForEach(
-			days.Keys,
-			dayName => {
-				int offset = 0;
-				string[] lines = ["", "", "", "", "", ""];
-				List<Types.Task> tasks_ = tasks.Where(x => x.StartDateTime.DayOfWeek == days[dayName]).ToList();
-				if (tasks_.Count == 0)
-					return;
-				foreach (Types.Task task in tasks_) {
-					if (task.running)
-						continue;
-					string[] compiledTask = CompileTask(task);
-					try {
-						Array.ConstrainedCopy(compiledTask, 0, lines, offset, compiledTask.Length);
-						query = $"{dayName}_hour_range_{offset + 1}";
-						value = DateTimeService.ToHourMinuteStringSinceMidnight(task.start) + " - " + DateTimeService.ToHourMinuteStringSinceMidnight(task.finish);
-						BufferValue(query, value);
-						query = $"{dayName}_hour_{offset + 1}";
-						value = DateTimeService.ToHourMinuteStringSinceMidnight(task.finish - task.start);
-						BufferValue(query, value);
-						offset += compiledTask.Length;
-					} catch (ArgumentOutOfRangeException) {
-						Console.WriteLine($"ran out of empty lines while inserting {compiledTask.Length} lines for day {dayName}");
-						Console.WriteLine($"description of task was:'{task.description}'");
-						break;
-					} catch (ArgumentException) {
-						Console.WriteLine($"ran out of empty lines while inserting {compiledTask.Length} lines for day {dayName}");
-						Console.WriteLine($"description of task was:'{task.description}'");
-						break;
-					}
-					totalWeekSeconds += task.finish - task.start;
+		foreach(var dayName in days.Keys) {
+			int offset = 0;
+			string[] lines = ["", "", "", "", "", ""];
+			List<Types.Task> tasks_ = tasks.Where(x => x.StartDateTime.DayOfWeek == days[dayName]).ToList();
+			if (tasks_.Count == 0)
+				return;
+			foreach (Types.Task task in tasks_) {
+				if (task.running)
+					continue;
+				string[] compiledTask = CompileTask(task);
+				try {
+					Array.ConstrainedCopy(compiledTask, 0, lines, offset, compiledTask.Length);
+					query = $"{dayName}_hour_range_{offset + 1}";
+					value = DateTimeService.ToHourMinuteStringSinceMidnight(task.start) + " - " + DateTimeService.ToHourMinuteStringSinceMidnight(task.finish);
+					BufferValue(query, value);
+					query = $"{dayName}_hour_{offset + 1}";
+					value = DateTimeService.ToHourMinuteStringSinceMidnight(task.finish - task.start);
+					BufferValue(query, value);
+					offset += compiledTask.Length;
+				} catch (ArgumentOutOfRangeException) {
+					Console.WriteLine($"ran out of empty lines while inserting {compiledTask.Length} lines for day {dayName}");
+					Console.WriteLine($"description of task was:'{task.description}'");
+					break;
+				} catch (ArgumentException) {
+					Console.WriteLine($"ran out of empty lines while inserting {compiledTask.Length} lines for day {dayName}");
+					Console.WriteLine($"description of task was:'{task.description}'");
+					break;
 				}
-				for (int i = 0; i < lines.Length; i++) {
-					query = $"{dayName}_line_{i + 1}";
-					BufferValue(query, lines[i]);
-				}
-
-			});
+				totalWeekSeconds += task.finish - task.start;
+			}
+			for (int i = 0; i < lines.Length; i++) {
+				query = $"{dayName}_line_{i + 1}";
+				BufferValue(query, lines[i]);
+			}
+		}
 		//foreach (string dayName in days.Keys) {
 		//}
 		query = $"total_hour";
